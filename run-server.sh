e93f581cb7f0d2149663906f8a8d1d2c3e39b170
muonceo <ceo@muon.co>
2024-03-19 18:46:49 +0900

    Fix script

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kill $(ps ax | grep muon-website-server | head -n 1 | awk '{ print $1 }')
 cd ~/dist/backend
-RUST_LOG="debug" nohup ./target/release/muon-website-server >> /tmp/muon-website-server.log &
+RUST_LOG="debug" nohup ./target/release/muon-website-server >> /tmp/muon-website-server.out.log 2>> /tmp/muon-website-server.err.log < /dev/null &
