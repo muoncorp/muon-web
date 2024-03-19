ce66b083d9868bba7880a2e3d646da173f09767c
muonceo <ceo@muon.co>
2024-03-19 18:20:41 +0900

    Update scripts

--- run-server.sh
+++ run-server.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-
-run_server () {
-    killall target/release/muon-website-server
-    cd backend
-    RUST_LOG="debug" nohup ~/.cargo/bin/cargo run --release >> /tmp/muon-website-server.log &
-}
-
-run_server
+kill $(ps ax | grep muon-website-server | head -n 1 | awk '{ print $1 }')
+cd dist/backend
+RUST_LOG="debug" nohup ./target/release/muon-website-server >> /tmp/muon-website-server.log &
