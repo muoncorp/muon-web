e12b46b09762a0184236ee249c772b020c1d6678
muonceo <muonceo@muon.co>
2025-01-03 15:59:44 +0900

    Update github action

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 APPDIR=/opt/muon-web
+cd $APPDIR
+
 mkdir -p logs
 TIMESTR=$(date -Iseconds)
 RUST_LOG="debug" nohup $APPDIR/muon-web-server \
 	>> $APPDIR/logs/muon-web-server_$TIMESTR.stdout.log \
 	2>> $APPDIR/logs/muon-web-server_$TIMESTR.stderr.log \
 	< /dev/null &
+
+echo muon-web-server started.
--- stop-server.sh
+++ stop-server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 kill $(ps ax | grep muon-web-server | head -n 1 | awk '{ print $1 }')
+echo muon-web-server stopped.
